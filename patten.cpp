b45d8a4804e2f900f5f7d97e3d133aa50d5172dd
pavanpriyank <pavankatiha@gmail.com>
2026-06-29 19:07:56 +0530

    loop problem1

---
+++ patten.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int n;
+   cin>>n;
+   
+   for(int j=1;j<=n;j++){
+    for(int i=1;i<=(n-j+1);i++){
+        cout<<"#";
+    }
+    cout<<endl;
+   }
+    
+    return 0;
+}
