43bc95912923560d36c9f76040ce49762e4d83fe
pavanpriyank <pavankatiha@gmail.com>
2026-06-29 12:31:16 +0530

    firstime

---
+++ cat.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // to print natural numbers from 1 to 10
+
+    // for
+   
+
+    // while
+   int num=1;
+   while(num <= 10){
+    if(num% 2!= 0){
+        cout<<num<<endl;
+    }
+     num++;
+   }
+    
+    //do while
+
+    return 0;
+}
