c3f929e166281f5285a44d74efaf8bd4ff0898f7
pavanpriyank <pavankatiha@gmail.com>
2026-07-01 20:03:28 +0530

    function

---
+++ func.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int add(int num1,int num2){
+int sum=(num1+num2);
+  return sum;
+}
+int main(){
+    int a=6;
+    int b=7;
+
+cout<<add(a,b)<<endl;
+return 0;
+}
