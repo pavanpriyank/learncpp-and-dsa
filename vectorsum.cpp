83ae701489ec65e2a94e980f2a6d3a714bf09f69
pavanpriyank <pavankatiha@gmail.com>
2026-07-09 16:42:36 +0530

    vector is complete

---
+++ vectorsum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+ int array[]={1,2,3,4,5,6};
+int sum=0;
+
+ for(int i=0;i<6;i++){
+    if(i%2==0){
+      sum+=array[i];
+    }
+     else{
+    sum-=array[i];
+    }
+ }
+ cout<<sum<<endl;
+
+ return 0;
+}
