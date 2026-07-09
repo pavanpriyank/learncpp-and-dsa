a67c10fa135b9b856d177a0da8f5d5403a748065
pavanpriyank <pavankatiha@gmail.com>
2026-07-09 17:17:30 +0530

    again array problems

---
+++ targetsum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int array[]={1,2,3,4,5,6};
+    
+    int targetsum=10;
+    int size=5;
+    int pairs=0;
+
+    for(int i=0;i<size;i++){
+    for(int j=i+1;j<size;j++){
+        if(array[i]+array[j]==targetsum){
+         pairs++;
+        }
+    }
+}
+cout<<pairs<<endl;
+
+    return 0;
+}
