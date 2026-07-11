9d6a11ef9978fc68cdd0e3169b175643c3a42cda
pavanpriyank <pavankatiha@gmail.com>
2026-07-11 11:48:05 +0530

    method to find second element in double value

--- secLargestElement.cpp
+++ secLargestElement.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include<climits>
 using namespace std;
-
-int largestElementIndex(int array[], int size){
+/*
+int largestElementIndex(int array[], int size)i{
     int max= INT_MIN;
     int maxindex =-1;
     for (int i = 0; i < size; i++){
@@ -13,17 +13,43 @@ int largestElementIndex(int array[], int size){
     }
     return maxindex;
 }
-
+*/                             
+int largestElementIndex(int array[], int size){ //this is another method to find second element in double value
+    int max= INT_MIN;
+    int second_max=INT_MAX;
+    for(int i=0;i<size;i++){
+        if(array[i]>max){
+            max=array[i];
+        }
+    }
+    for(int i=0;i<size;i++){
+        if(array[i]>second_max && array[i]!=max){
+            second_max=array[i];
+        }
+    }
+return second_max;
+}
 int main(){
+    int array[]={1,2,3,6,5,6};                 
+    int n=6;                               
 
-    int array[]={1,2,3,4,5,6};                 //for only single/unique type value
-    int n=6;
+ /*
+int array[]={1,2,3,6,5,6};                 //for only single/unique type value
+    int n=6;                               
     int indexoflargest= largestElementIndex(array,n);
     cout<<array[indexoflargest]<<endl;
     array[indexoflargest]=-1;
+
+//  int largestelement=array[indexoflargest];     //this is for double value to find second largest element
+//  for(int i=0;i<n;i++){
+//     if (array[i]==largestelement){
+//  array[i]=-1;
+//}
+//    }
     int secondlargestelement=largestElementIndex(array,n);
     cout<<array[secondlargestelement]<<endl;
+*/   
 
+cout<<(array , n)<<endl;
     return 0;
-
 }
