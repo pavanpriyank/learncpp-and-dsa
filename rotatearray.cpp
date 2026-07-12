866fba81153a2916e180b1eca904c815e791cadb
pavanpriyank <pavankatiha@gmail.com>
2026-07-12 22:21:15 +0530

    again rotatearray

--- rotatearray.cpp
+++ rotatearray.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;                              //ask
 int main(){
+
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+
+    int k=2;
+    k=k%v.size();
+
+    reverse(v.begin(),v.end());
+    reverse(v.begin(),v.begin()+k);
+    reverse(v.begin()+k,v.end());
+
+    for (int a:v){
+        cout<<a<<" ";
+    }
+    cout<<endl;
+
+ /*
  int array[]={1,2,3,4,5,6};
  int n=6;
  int k=2;
@@ -21,5 +44,6 @@ for (int i=0; i<n;i++){
     cout<<ansarray[i]<<" ";
 }
 cout<<endl;
+*/   
     return 0;
 }
