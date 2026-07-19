7891afb6aed456c707d146c0e44033271501d983
pavanpriyank <pavankatiha@gmail.com>
2026-07-19 16:11:58 +0530

    pattern 9

---
+++ pattern9.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=(n-i);j++){
+            cout<<" ";
+        }
+        if (i==n){
+        for(int j=1;j<=(2*i-1);j++){
+            cout<<i;
+        }
+    }
+    else{
+        cout<<i;
+        if (i>1){
+            for(int j=1;j<=(2*i-3);j++){
+                cout<<" ";
+            }
+            cout<<i;
+        }
+    }
+        cout<<endl;
+    }
+    return 0;
+}
