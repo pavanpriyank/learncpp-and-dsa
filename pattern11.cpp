388e4642d3af32fd35a04d43415dfe4915d56972
pavanpriyank <pavankatiha@gmail.com>
2026-07-21 14:44:51 +0530

    some changes in pattern

--- pattern11.cpp
+++ pattern11.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-
+ /*   int n,m;
+    cin>>n>>m;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=m;j++){
+        cout<<"*";
+    }
+    cout<<"\n";
+}
+*/
+int n=5;
     //for loop to do some task for each line
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
