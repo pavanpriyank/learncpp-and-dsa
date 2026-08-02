243fbf2391400ca1735ee94d39a1da70f68767cf
pavanpriyank <pavankatiha@gmail.com>
2026-08-02 19:42:24 +0530

    rotation of matrix

--- matrixTranspose.cpp
+++ matrixTranspose.cpp
@@ -21,7 +21,7 @@ int main(){
 
       for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-             Transpose[i][j]=array[j][i];
+            Transpose[i][j]=array[j][i];
         }
     }
      for(int i=0;i<m;i++){
