38ef99ec57ef2edec0b7ecc631b6949dcbfc41f3
pavanpriyank <pavankatiha@gmail.com>
2026-08-20 21:29:24 +0530

    is Number is Palidnrome or not

---
+++ isNumberIsPalidnrome.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+bool f(int num, int *temp){
+    //base case
+if(num >= 0 and num <= 9){
+    int lastDigitofTemp = (*temp)%10;
+    (*temp) /= 10;
+    return (num == lastDigitofTemp);
+}
+bool result = (f(num/10,temp) and (num%10) == ((*temp)% 10));
+(*temp) /= 10;
+return result;
+}
+
+int main(){
+    int num = 1441;
+    int anotherNum = num;
+    int *temp = &anotherNum;
+    cout<<f(num, temp);
+    return 0;
+}
