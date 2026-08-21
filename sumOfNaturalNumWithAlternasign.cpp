61bcffd9ef4fb4b05554bfc2ec5160f4af9db72c
pavanpriyank <pavankatiha@gmail.com>
2026-08-21 21:56:36 +0530

    sum of natural number with alternate sign

---
+++ sumOfNaturalNumWithAlternasign.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int f(int n){
+    //base case
+    if(n == 0) return 0;
+    //assumption 
+   return f(n-1) + ((n%2 == 0) ? (-n) : (n)); // calculate sum of first n-1 natural num with alternate, and we will add contri of n
+}
+int main(){
+
+    cout<<f(3)<<"\n";
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,9 @@
 
+    if(n == 0) return 0;
+    //assumption 
+    return f(n-1) + ((n%2 == 0) ? (-n) : (n)); // calculate sum of first n-1 natural num with alternate, and we will add contri of n
+}
+int main(){
 
-        cout << endl;
-    }
+    cout<<f(3)<<"\n";
+    return 0;
