aa6def403db29c6e90978394a6251b57c586e4fa
pavanpriyank <pavankatiha@gmail.com>
2026-09-21 19:24:42 +0530

    first occurence of target binary Search

---
+++ first_occcurence_of_target_binarySearch.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int firstoccurence(vector<int> &input , int target){
+    int lo = 0; 
+    int hi = input.size()-1;
+    int ans = -1;
+    while(lo <= hi){
+        int mid = lo + (hi - lo) / 2;
+        if(input[mid] == target){
+            ans = mid;
+            hi = mid - 1;
+        }
+        else if(input[mid] > target){
+            hi = mid - 1;
+        }
+        else{
+            lo = mid + 1;
+        }
+    }
+    return ans;
+
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr;
+    for(int i=0;i< n;i++){
+        int x;
+        cin>>x;
+        arr.push_back(x);
+    }
+
+    int target;
+    cin>>target;
+    cout<<firstoccurence(arr, target)<<endl;
+    
+    return 0;
+}
